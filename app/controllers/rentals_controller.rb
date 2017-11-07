dadc4c1633f9d038da5f88c8f5236ea3684f41be
Clayton <c_w_serious-github@yahoo.com.au>
2017-11-08 09:54:42 +1100

    Rentals have start & end datetimes

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,8 +2,12 @@ class RentalsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # Get the rentals that the logged in user has made
-    @rentals = Rental.find_by(tool_id: @tool.id)
+    # Get the rentals for current tool
+    rentals_current_tool = Rental.where(tool_id: params[:tool_id])
+    @rentals = rentals_current_tool
+
+    @rental = Rental.new
+    
   end
   
   def show
@@ -17,17 +21,24 @@ class RentalsController < ApplicationController
   end
   
   def create
+    
+    
+
     rental_params = params.require(:rental).permit(:tool_id, :hirer_id, :charge_identifier, :start_of_hire, :end_of_hire)
     
     # New rental to save to database
-    @rental = Rental.new
+    @rental = Rental.new(rental_params)
     # The tool_id comes from the form (hidden_tag :tool_id)
     @rental.tool = Tool.find(rental_params[:tool_id])
     @rental.hirer = current_user
-      
+    
+    #x = rental_params[:tool_id]
+    #tool = Tool.find(x)
+
     # Amount in cents
     @amount = @rental.tool.cost_of_hire_cents
-    
+    #@amount = tool.cost_of_hire_cents
+
     customer = Stripe::Customer.create(
       :email => current_user.email,
       :source  => params[:stripeToken]
@@ -36,16 +47,17 @@ class RentalsController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @amount,
-      :description => @rental.tool.name,
+      :description => @rental.tool.name, #tool.name, 
       :currency    => 'aud'
     )
   
     @rental.charge_identifier = charge.id
     @rental.save
-    redirect_to rentals_path
+    redirect_to tools_path
     
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_rental_path(tool_id: @rental.tool.id)
+      #redirect_to new_rental_path(tool_id: tool.id)
   end
 end
