564be7db671c61f6296887f8ed2406d710450e25
Clayton <c_w_serious-github@yahoo.com.au>
2017-11-09 09:18:24 +1100

    Pundit working for tools

--- app/policies/tool_policy.rb
+++ app/policies/tool_policy.rb
@@ -1,16 +1,7 @@
 class ToolPolicy < ApplicationPolicy
-    def show?
-      # If current user owns current tool
-      if user.id == record.owner.id
-        true
-      else
-        false
-      end
-    end
-  
     def update?
       # If current user owns current tool
-      if user.id == record.owner.id
+      if user.id == record.owner_id
         true
       else
         false
